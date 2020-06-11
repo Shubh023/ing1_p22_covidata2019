5cb6da6faa53bbe2ab734d5639d1680e9baa4f84
Akram ZABAT <akram.zabat@epita.fr>
2020-06-12 01:32:31 +0200

    [DATA ACTIVITY] LAYOUT FINISHED

---
+++ src/app/src/main/java/fr/epita/android/covidata2019/DataActivitiy.kt
@@ -0,0 +1,13 @@
+package fr.epita.android.covidata2019
+
+import android.app.ActivityOptions
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataActivitiy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.data_activitiy)
+
+    }
+}
--- src/app/src/main/java/fr/epita/android/covidata2019/MainActivity.kt
+++ src/app/src/main/java/fr/epita/android/covidata2019/MainActivity.kt
@@ -1,11 +1,18 @@
 package fr.epita.android.covidata2019
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainDataBtn.setOnClickListener {
+            startActivity(Intent(this, DataActivitiy::class.java),
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
     }
 }
