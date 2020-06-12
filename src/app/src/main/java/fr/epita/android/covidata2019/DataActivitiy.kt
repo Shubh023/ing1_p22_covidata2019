f5040c8f4688242fc1878f0b1111bca1bd823aff
Akram ZABAT <akram.zabat@epita.fr>
2020-06-12 02:00:15 +0200

    [CREDITS] ADDED

--- src/app/src/main/java/fr/epita/android/covidata2019/DataActivitiy.kt
+++ src/app/src/main/java/fr/epita/android/covidata2019/DataActivitiy.kt
@@ -3,11 +3,16 @@ package fr.epita.android.covidata2019
 import android.app.ActivityOptions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.data_activitiy.*
 
 class DataActivitiy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.data_activitiy)
+
+        dataConfirmedWorld.text = "0"
+        dataDeathsWorld.text = "0"
+        dataRecoveredWorld.text = "0"
 
     }
 }
