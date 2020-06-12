59fc872811d83453828c4832c33c8bdf90ca9e5f
Akram ZABAT <akram.zabat@epita.fr>
2020-06-12 16:20:50 +0200

    [CALENDAR] drawables

--- src/app/src/main/java/fr/epita/android/covidata2019/CalendarActivity.kt
+++ src/app/src/main/java/fr/epita/android/covidata2019/CalendarActivity.kt
@@ -2,10 +2,15 @@ package fr.epita.android.covidata2019
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calendar.*
 
 class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+        calendarConfirmed.text = "0"
+        calendarDeaths.text = "0"
+        calendarRecovered.text = "0"
     }
 }
