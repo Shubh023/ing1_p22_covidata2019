3d2f65dda1362abe764a3affb6ec51aa9c43dad4
Akram ZABAT <akram.zabat@epita.fr>
2020-06-12 16:21:14 +0200

    [GRAPH] drawables

--- src/app/src/main/java/fr/epita/android/covidata2019/DataActivitiy.kt
+++ src/app/src/main/java/fr/epita/android/covidata2019/DataActivitiy.kt
@@ -1,14 +1,13 @@
 package fr.epita.android.covidata2019
 
-import android.app.ActivityOptions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.data_activitiy.*
+import kotlinx.android.synthetic.main.activity_data.*
 
 class DataActivitiy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.data_activitiy)
+        setContentView(R.layout.activity_data)
 
         dataConfirmedWorld.text = "0"
         dataDeathsWorld.text = "0"
