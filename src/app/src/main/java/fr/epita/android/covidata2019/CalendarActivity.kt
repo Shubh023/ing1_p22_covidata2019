6e1043f670852d0e68d53d6326f736cae41c18a2
Akram ZABAT <akram.zabat@epita.fr>
2020-06-12 16:18:55 +0200

    [CALENDAR] activity added

---
+++ src/app/src/main/java/fr/epita/android/covidata2019/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package fr.epita.android.covidata2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
