8325d346b80f0ede88fdf5ec3de38ad68cbd6964
Akram ZABAT <akram.zabat@epita.fr>
2020-06-15 19:23:41 +0200

    EASTEREGG ON CLICK LISTENER

--- src/app/src/main/java/fr/epita/android/covidata2019/MainActivity.kt
+++ src/app/src/main/java/fr/epita/android/covidata2019/MainActivity.kt
@@ -4,9 +4,11 @@ import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
+    var easterEggCounter : Int = 6
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +27,21 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, CalendarActivity::class.java)
                 .putExtra("Country", "France"),
                 ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
+
+        //easter egg
+        logoEpita.setOnClickListener {
+            easterEggCounter--;
+            if (easterEggCounter == 0)
+                Toast.makeText(this, "Well Done ! Welcome", Toast.LENGTH_SHORT).show()
+            else if (easterEggCounter < 4)
+                Toast.makeText(this, "Remaining ${easterEggCounter.toString()} clicks", Toast.LENGTH_SHORT).show()
+            if (easterEggCounter <= 0)
+            {
+                // to change to misteryActivity
+                startActivity(Intent(this, DataActivity::class.java))
+                easterEggCounter = 6
+            }
         }
     }
 }
